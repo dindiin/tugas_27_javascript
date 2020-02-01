0484d5fb98d73be6e5cb684d7f4777931117ce9a
Nurdiansyah <nurdiansyah.st@gmail.com>
2020-02-01 11:39:53 +0700

    Tugas 27 Javscript
    Pengumpulan tugas Niomic

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+function panggilTugas() {
+    var a = "No";
+    var b = "rumah saya";
+    var c = 30;
+    var d = "A";
+    var e = "Jalan pangeran Tirtayasa No";
+    var f = 70;
+
+    cc = String(c);
+    ff = String(f);
+
+    console.log(a+" "+b+" "+cc+" "+d);
+    console.log(e+" "+f);
+}
+panggilTugas();
